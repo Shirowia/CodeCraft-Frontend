72a03e84b87618a5f51fbbb62580e93ec9087408
Ami <joieannegarzo@gmail.com>
2025-02-24 09:26:56 +0800

    puts navbar in the same container as components

--- src/App.js
+++ src/App.js
@@ -1,23 +1,26 @@
 import React from 'react';
 import 'bootstrap/dist/css/bootstrap.min.css';
-import { BrowserRouter as Router, Route, Routes } from 'react-router-dom';
+import { BrowserRouter as Router, Route, Routes, Link, Navigate } from 'react-router-dom';
 import Signup from './components/Signup';
 import Login from './components/Login';
-import { Link } from 'react-router-dom';
 import Profile from './components/Profile';
-import { Navigate } from 'react-router-dom';
 import { getAuth } from 'firebase/auth';
+import '../styles/general.css';
 
 const App = () => {
   const PrivateRoute = ({ children }) => {
     const auth = getAuth();
     return auth.currentUser ? children : <Navigate to="/login" />;
   };
+
   return (
     <Router>
-          <nav className="navbar navbar-expand-lg navbar-light bg-light">
+      {/* Main container for the entire app */}
+      <div className="container mt-4">
+        {/* Navbar */}
+        <nav className="navbar navbar-expand-lg navbar-light bg-light rounded shadow-sm">
           <div className="container-fluid">
-            <Link className="navbar-brand" to="/">Home</Link>
+            <Link className="navbar-brand" to="/">CodeCraft</Link>
             <div className="collapse navbar-collapse">
               <ul className="navbar-nav me-auto">
                 <li className="nav-item">
@@ -26,24 +29,32 @@ const App = () => {
                 <li className="nav-item">
                   <Link className="nav-link" to="/login">Login</Link>
                 </li>
+                <li className="nav-item">
+                  <Link className="nav-link" to="/profile">Profile</Link>
+                </li>
               </ul>
             </div>
           </div>
         </nav>
-      <Routes>
-      <Route path="/profile"
-            element={
-              <PrivateRoute>
-                <Profile />
-              </PrivateRoute>
-            }
-          />
-        <Route path="/signup" element={<Signup />} />
-        <Route path="/login" element={<Login />} />
-      </Routes>
+
+        {/* Page Content */}
+        <div className="mt-4 p-4 bg-white rounded shadow-sm">
+          <Routes>
+            <Route
+              path="/profile"
+              element={
+                <PrivateRoute>
+                  <Profile />
+                </PrivateRoute>
+              }
+            />
+            <Route path="/signup" element={<Signup />} />
+            <Route path="/login" element={<Login />} />
+          </Routes>
+        </div>
+      </div>
     </Router>
   );
-
 };
 
 export default App;

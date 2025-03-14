567cf9a03eb980882e22e9062374fbe9839ed19b
Paul <155648097+Shirowia@users.noreply.github.com>
2025-03-14 18:10:58 +0800

    fixed dependencies retry again

--- src/firebase/firebase.js
+++ src/firebase/firebase.js
@@ -1,12 +1,13 @@
 import { initializeApp } from 'firebase/app';
 import { getAuth, setPersistence, browserLocalPersistence } from 'firebase/auth';
 import { getFirestore } from 'firebase/firestore';
+import { getStorage } from 'firebase/storage';
 
 const firebaseConfig = {
   apiKey: "AIzaSyAC9zxvPZxVjAUjMSA753ckyhF5ONjKKhs",
   authDomain: "codecraft-29e84.firebaseapp.com",
   projectId: "codecraft-29e84",
-  storageBucket: "codecraft-29e84.firebasestorage.app",
+  storageBucket: "codecraft-29e84.appspot.com", // Fixed incorrect storage bucket URL
   messagingSenderId: "655049358972",
   appId: "1:655049358972:web:d0731b4acd1aca424a13f9",
   measurementId: "G-STKW1CVXJZ"
@@ -16,7 +17,9 @@ const firebaseConfig = {
 const app = initializeApp(firebaseConfig);
 const auth = getAuth(app);
 const db = getFirestore(app);
+const storage = getStorage(app);
 
+// Set authentication persistence
 setPersistence(auth, browserLocalPersistence)
   .then(() => {
     console.log('Persistence set to local');
@@ -25,4 +28,4 @@ setPersistence(auth, browserLocalPersistence)
     console.error('Failed to set persistence:', error);
   });
 
-export { auth, db };
+export { app, auth, db, storage };
